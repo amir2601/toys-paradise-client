1520522746b5bd3243c60faae06fbd3826d158d4
amir260101~ <amir26hossin@gmail.com>
2023-05-18 15:29:55 +0600

    login page design done

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,7 +1,19 @@
 import React from 'react';
 import loginImg from '../../assets/login/login2.png'
+import { Link } from 'react-router-dom';
 
 const Login = () => {
+
+    const handleLogin = event => {
+        event.preventDefault();
+        const form = event.target;
+        const email = form.email.value;
+        const password = form.password.value;
+        
+        const user = {Email: email, Password: password}
+        console.log(user);
+    }
+
     return (
         <div>
             <div className="hero min-h-[80vh] bg-accent rounded-lg">
@@ -12,24 +24,26 @@ const Login = () => {
                     <div className="card flex-shrink-0 max-w-sm shadow-2xl bg-base-100 md:w-1/2">
                         <div className="card-body">
                             <h1 className="text-4xl font-bold">Login</h1>
-                            <div className="form-control">
+                            <form onSubmit={handleLogin}>
+                                <div className="form-control">
+                                    <label className="label">
+                                        <span className="label-text">Email</span>
+                                    </label>
+                                    <input type="email" name='email' placeholder="email" className="input input-bordered" required />
+                                </div>
+                                <div className="form-control">
+                                    <label className="label">
+                                        <span className="label-text">Password</span>
+                                    </label>
+                                    <input type="password" name='password' placeholder="password" className="input input-bordered" required />
+                                </div>
+                                <div className="form-control mt-6">
+                                    <input className="btn btn-primary" type="submit" value="Login" />
+                                </div>
                                 <label className="label">
-                                    <span className="label-text">Email</span>
+                                    <p className='label-text-alt '>Don't have an account? <Link to='/register' className="link link-hover">Register</Link> </p>
                                 </label>
-                                <input type="email" name='email' placeholder="email" className="input input-bordered" required />
-                            </div>
-                            <div className="form-control">
-                                <label className="label">
-                                    <span className="label-text">Password</span>
-                                </label>
-                                <input type="password" name='password' placeholder="password" className="input input-bordered" required />
-                            </div>
-                            <div className="form-control mt-6">
-                                <input className="btn btn-primary" type="submit" value="Login" />
-                            </div>
-                            <label className="label">
-                                <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
-                            </label>
+                            </form>
                         </div>
                     </div>
                 </div>
